a01fe60b4f11aaf1ab4c9f383f2c34155010217d
Saumaun <svahedipour1@gmail.com>
2022-05-17 22:28:25 -0500

    added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'gray-sidebar': '#2F3136',
+        'gray-message': '#40444B',
+        'gray-user': '#292B2F',
+        'gray-chat': '#36393F',
+      }
+    },
+  },
+  plugins: [],
+};
