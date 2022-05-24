dd7fc6bc9e37fc97a1ea48a25cb08a16620b504f
Saumaun <svahedipour1@gmail.com>
2022-05-23 21:53:44 -0500

    created custom hook for sidebar channel

---
+++ customhooks.ts
@@ -0,0 +1,21 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function useComponentVisible(initialVisible: boolean) {
+  const [isComponentVisible, setIsComponentVisible] = useState(initialVisible);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      setIsComponentVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  }, []);
+
+  return { ref, isComponentVisible, setIsComponentVisible };
+}
