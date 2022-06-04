ddb8ac704658a5b184110fbfccaacb2cb6dd1df5
Saumaun <svahedipour1@gmail.com>
2022-06-04 18:39:20 -0500

    added route for discord servers

---
+++ pages/[server]/index.tsx
@@ -0,0 +1,63 @@
+import { Channel } from "@prisma/client";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import Chat from "../../components/Chat";
+import Sidebar from "../../components/Sidebar";
+import StateProvider, { initialState, reducer } from "../../context/StateProvider";
+import prisma from "../../prisma";
+
+interface Props {
+  textChannels: Channel[];
+  voiceChannels: Channel[];
+}
+
+const Server = ({ textChannels, voiceChannels }: Props) => {
+  const router = useRouter();
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push('/')
+    },
+  });
+  
+  if(status === 'loading'){
+    <h1>Loading...</h1>
+  }
+
+  const refreshData = () => {
+    router.replace(router.asPath);
+  };
+
+  if(status === 'authenticated'){
+  return (
+    <StateProvider reducer={reducer} initialState={initialState}>
+      <div className="flex h-screen">
+        <Sidebar
+          textChannels={textChannels}
+          voiceChannels={voiceChannels}
+          refreshData={refreshData}
+        />
+        <Chat />
+      </div>
+    </StateProvider>
+  );
+ };
+}
+
+export async function getServerSideProps() {
+  const textChannels = await prisma.channel.findMany({
+    where: {
+      type: "text",
+    },
+  });
+  const voiceChannels = await prisma.channel.findMany({
+    where: {
+      type: "voice",
+    },
+  });
+  return {
+    props: { textChannels, voiceChannels },
+  };
+}
+
+export default Server;
