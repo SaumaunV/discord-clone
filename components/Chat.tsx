9ae517da05769200849daa761b462b901c4ac89c
Saumaun <svahedipour1@gmail.com>
2022-06-05 00:02:21 -0500

    added signout button to chat component

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,3 +1,4 @@
+import { signOut, useSession } from "next-auth/react";
 import { FaHashtag } from "react-icons/fa";
 import { IoMdAddCircle } from "react-icons/io";
 import { useStateValue } from "../context/StateProvider";
@@ -5,14 +6,25 @@ import Message from "./Message";
 
 function Chat() {
   const [{ channel }] = useStateValue();
+  const {data: session} = useSession();
+
   return (
-    <div className="bg-gray-chat flex flex-col flex-[0.85]">
-      <div className="flex items-center h-12 p-3 w-full text-white font-medium border-b-[1px] border-neutral-800 border-solid shadow-sm">
-        <FaHashtag className="mr-2 text-gray-icons text-xl" /> {channel.name}
+    <div className="bg-gray-chat flex flex-col flex-1">
+      <div className="flex items-center justify-between h-12 p-3 w-full text-white font-medium border-b-[1px] border-neutral-800 border-solid shadow-sm">
+        <div className="flex items-center">
+          <FaHashtag className="mx-2 text-gray-icons text-xl" />
+          <span>{channel.name}</span>
+        </div>
+        <button
+          className="mr-3 bg-gray-user py-1 px-2 rounded text-gray-sidetext hover:bg-gray-900 hover:text-gray-chat-text transition"
+          onClick={() => signOut({ callbackUrl: `/` })}
+        >
+          Sign out
+        </button>
       </div>
 
       <div className="flex flex-col flex-1 justify-end">
-          <Message />
+        <Message />
       </div>
 
       <form className="flex m-4 mb-6 items-center bg-gray-message rounded-lg  px-4">
