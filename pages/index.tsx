b4f608838f939f73eda18620afa482bd15b0e71e
Saumaun <svahedipour1@gmail.com>
2022-06-14 22:46:40 -0500

    updated main login page route based on user

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 
 const Home = () => {
+  const router = useRouter();
+  const {data: session, status } = useSession();
 
+  if(status === 'authenticated'){
+    router.push('/@me');
+  }
+  if(status === 'loading'){
+    <h1>Loading...</h1>
+  }
+  if(status === 'unauthenticated') {
    return (
-
      <div className="grid place-items-center h-screen w-full">
        <div>
          <img
@@ -14,13 +23,13 @@ const Home = () => {
          />
        </div>
        <button className="w-80 font-semibold text-white bg-blue-button px-4 py-2 rounded-[3px] hover:bg-blue-button-hover transition"
-       onClick={() => signIn('google', {callbackUrl: `/1`})}
+       onClick={() => signIn('google')}
        >
          Sign in with Google
        </button>
      </div>
    );
-
+  }
 
 }
 
