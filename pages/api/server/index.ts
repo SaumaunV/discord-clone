1c65d15bc18992471d14ad9d1ba8c32b03d2321b
Saumaun <svahedipour1@gmail.com>
2022-06-26 17:14:52 -0500

    server index api route
    added functionality to link user with server when user joins a server

--- pages/api/server/index.ts
+++ pages/api/server/index.ts
@@ -1,9 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../prisma";
 
-type DataType = {
+type CreateServerData = {
     name: string,
     userId: string,
+}
+
+type JoinServerData = {
+  serverId: string,
+  userId: string,
 }
 
 export default async function handler(
@@ -13,8 +18,8 @@ export default async function handler(
 
   if (req.method === "POST") {
     try {
-      const data = JSON.parse(req.body) as DataType;
-      await prisma.server.create({
+      const data = JSON.parse(req.body) as CreateServerData;
+      const newServer = await prisma.server.create({
         data: {
           name: data.name,
           users: {
@@ -28,8 +33,40 @@ export default async function handler(
               { name: "General", type: "voice" },
             ],
           },
+          owner: {
+            connect: {
+              id: data.userId
+            }
+          }
         },
       });
+      res.status(200).json({ message: "submitted successfully", serverId: newServer.id });
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  }
+
+  if(req.method === 'PUT') {
+    try {
+      const data = JSON.parse(req.body) as JoinServerData;
+      const servers = await prisma.server.findMany();
+      const server = servers.find(server => server.id === data.serverId);
+      if(server) {
+        await prisma.server.update({
+          where: {
+            id: data.serverId,
+          },
+          data: {
+            users: {
+              connect: {
+                id: data.userId,
+              },
+            },
+          },
+        });
+      } else {
+        res.status(400).json({message: "could not find server"});
+      }       
       res.status(200).json({ message: "submitted successfully" });
     } catch (error) {
       res.status(400).json({ error });
