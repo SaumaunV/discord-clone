fd103d750bd3cecafe854df4d81cd6b694215f59
Saumaun <svahedipour1@gmail.com>
2022-06-26 17:05:30 -0500

    updated api route for creating channels

--- pages/api/channel/index.ts
+++ pages/api/channel/index.ts
@@ -1,25 +1,29 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../prisma";
 
+type DataType = {
+  name: string,
+  type: string,
+  serverId: string,
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // if (req.method === "GET") {
-  //   try {
-  //     //const data = JSON.parse(req.body);
-  //     const channels = await prisma.channel.findMany();
-  //     res.status(200).json({ channels });
-  //   } catch (error) {
-  //     res.status(400).json({ error });
-  //   }
-  // }
-
   if (req.method === "POST") {
     try {
-      const data = JSON.parse(req.body);
+      const data = JSON.parse(req.body) as DataType;
       await prisma.channel.create({
-        data,
+        data: {
+          name: data.name,
+          type: data.type,
+          server: {
+            connect: {
+              id: data.serverId
+            }
+          }
+        }
       });
       res.status(200).json({ message: "submitted successfully" });
     } catch (error) {
