4debad166524c94155f63d6e06a37060d31974b1
Saumaun <svahedipour1@gmail.com>
2022-06-30 19:44:57 -0500

    userChannel api route
    added delete method that removes server channel after user leaves server

--- pages/api/channel/userChannel.ts
+++ pages/api/channel/userChannel.ts
@@ -18,6 +18,7 @@ export default async function handler(
             serverId: data.serverId
         }
       })
+      const channelIndex = channels.findIndex(channel => channel.type === 'text');
       await prisma.user.update({
         where: {
             id: data.userId
@@ -25,9 +26,39 @@ export default async function handler(
         data: {
             selectedchannels: {
                 connect: {
-                    id: channels[0].id,
+                    id: channels[channelIndex].id,
                 }
             },
+        },
+      });
+      res.status(200).json({ message: "submitted successfully" });
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+  }
+
+  if(req.method === 'DELETE') {
+    try {
+      const data = JSON.parse(req.body) as DataType;
+      const user = await prisma.user.findUnique({
+        where: {
+          id: data.userId,
+        },
+        include: {
+          selectedchannels: true
+        }
+      });
+      const channelId = user?.selectedchannels.find(channel => channel.serverId === data.serverId)!.id;
+      await prisma.user.update({
+        where: {
+          id: data.userId,
+        },
+        data: {
+          selectedchannels: {
+            disconnect: {
+              id: channelId,
+            },
+          },
         },
       });
       res.status(200).json({ message: "submitted successfully" });
